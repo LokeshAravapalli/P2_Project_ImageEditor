e6e602f8be914a9b2de2e2a192c9fe6799543b80
Pradyumna G <116896490+PradyumnaG@users.noreply.github.com>
2024-01-02 11:46:38 +0530

    Added comments

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementations/GaussianBlurEffect.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementations/GaussianBlurEffect.java
@@ -7,21 +7,21 @@ import com.iiitb.imageEffectApplication.libraryInterfaces.*;
 import com.iiitb.imageEffectApplication.libraryInterfaces.Pixel;
 import com.iiitb.imageEffectApplication.service.*;
 
-public class GaussianBlurEffect implements SingleValueParameterizableEffect {
+public class GaussianBlurEffect implements SingleValueParameterizableEffect { //implements SingleValueParameterizableEffect interface
 
     private float radius;
 
-    public void setParameterValue(float parameterValue) throws IllegalParameterException {
-        if (parameterValue < 0 || parameterValue > 50) {
+    public void setParameterValue(float parameterValue) throws IllegalParameterException {// this function sets parameter value
+        if (parameterValue < 0 || parameterValue > 50) {//throws exception is value is out of range(0,50)
             throw new IllegalParameterException("Radius value must be between 0.0 and 50.0");
         }
-        this.radius = parameterValue;
+        this.radius = parameterValue;//setting parameter value
     }
 
-    public Pixel[][] apply(Pixel[][] imageFile, String fileName, LoggingService loggingService) {
+    public Pixel[][] apply(Pixel[][] imageFile, String fileName, LoggingService loggingService) {//this functions applies gaussian blur effect
         String optionValues = String.valueOf(radius);
-        loggingService.addLog(fileName,"GaussianBlur",optionValues);
-        imageFile = GaussianBlurInterface.applyGaussianBlur(imageFile,radius);
+        loggingService.addLog(fileName,"GaussianBlur",optionValues);//adding to logs
+        imageFile = GaussianBlurInterface.applyGaussianBlur(imageFile,radius);//applying effect
         return imageFile;
     }
 }
