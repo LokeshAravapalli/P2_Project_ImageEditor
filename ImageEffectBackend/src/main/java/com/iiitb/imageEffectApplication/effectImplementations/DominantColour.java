d99243cc130735862493be7053afc839c97c3ab7
Pradyumna G <116896490+PradyumnaG@users.noreply.github.com>
2024-01-02 11:43:17 +0530

    Added comments

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementations/DominantColour.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementations/DominantColour.java
@@ -4,7 +4,7 @@ import com.iiitb.imageEffectApplication.baseEffects.PhotoEffect;
 import com.iiitb.imageEffectApplication.libraryInterfaces.*;
 import com.iiitb.imageEffectApplication.service.*;
 
-public class DominantColour implements PhotoEffect{// implemets Photo Effect
+public class DominantColour implements PhotoEffect{// implemets Photo Effect interface
     public Pixel[][] apply(Pixel[][] imageFile, String fileName, LoggingService loggingService) {//function applies dominant colour effect
         //Dominant colour effect has no parameter value
         loggingService.addLog(fileName,"Dominant Colour","no value");//adding to logs
