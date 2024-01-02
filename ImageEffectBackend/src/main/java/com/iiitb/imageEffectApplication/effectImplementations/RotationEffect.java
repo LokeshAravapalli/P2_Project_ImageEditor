27420b5e27c5999f0bcb6c3e5c8f874716a77383
Pradyumna G <116896490+PradyumnaG@users.noreply.github.com>
2024-01-02 13:08:52 +0530

    Added comments

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementations/RotationEffect.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementations/RotationEffect.java
@@ -6,21 +6,21 @@ import com.iiitb.imageEffectApplication.exception.IllegalParameterException;
 import com.iiitb.imageEffectApplication.libraryInterfaces.*;
 import com.iiitb.imageEffectApplication.service.*;
 
-public class RotationEffect implements SingleValueDiscreteEffect{
+public class RotationEffect implements SingleValueDiscreteEffect{//implements SingleValueDiscreteEffect interface
 
     private int rotationValue;
 
-    public void setParameterValue(int parameterValue) throws IllegalParameterException {
-        if (parameterValue!=0 && parameterValue!=1 && parameterValue!=2 && parameterValue!=3) {
+    public void setParameterValue(int parameterValue) throws IllegalParameterException {//sets parameter value for rotation effect
+        if (parameterValue!=0 && parameterValue!=1 && parameterValue!=2 && parameterValue!=3) {//throws exception if parameter values are not in the set : {0,1,2,3}
             throw new IllegalParameterException("Rotation value must be 0 or 90 or 180 or 270");
         }
-        this.rotationValue = parameterValue;
+        this.rotationValue = parameterValue;//setting parameter value
     }
 
-    public Pixel[][] apply(Pixel[][] imageFile, String fileName, LoggingService loggingService) {
+    public Pixel[][] apply(Pixel[][] imageFile, String fileName, LoggingService loggingService) {//this function applies rotation effect
         String optionValues = String.valueOf(rotationValue*90);
-        loggingService.addLog(fileName,"Rotation",optionValues);
-        imageFile = RotationInterface.applyRotation(imageFile,rotationValue);
+        loggingService.addLog(fileName,"Rotation",optionValues);//adding to logs
+        imageFile = RotationInterface.applyRotation(imageFile,rotationValue);//applying the effect
         return imageFile;
     }
 }
