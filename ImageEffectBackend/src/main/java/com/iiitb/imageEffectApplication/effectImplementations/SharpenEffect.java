4a6902e7bd8a8ea79b41e77c9d58d92f24b9ca25
Pradyumna G <116896490+PradyumnaG@users.noreply.github.com>
2024-01-02 13:12:17 +0530

    Added comments

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementations/SharpenEffect.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementations/SharpenEffect.java
@@ -7,21 +7,21 @@ import com.iiitb.imageEffectApplication.libraryInterfaces.*;
 import com.iiitb.imageEffectApplication.libraryInterfaces.Pixel;
 import com.iiitb.imageEffectApplication.service.*;
 
-public class SharpenEffect implements SingleValueParameterizableEffect {
+public class SharpenEffect implements SingleValueParameterizableEffect {//implements SingleValueParameterizableEffect interface
 
     private float sharpenValue;
 
-    public void setParameterValue(float parameterValue) throws IllegalParameterException {
-        if (parameterValue < 0.0f || parameterValue > 100.0f) {
+    public void setParameterValue(float parameterValue) throws IllegalParameterException {//this function sets the parameter value
+        if (parameterValue < 0.0f || parameterValue > 100.0f) {//throws exception if parameter value is out of range(0,100)
             throw new IllegalParameterException("Sharpen value must be between 0.0 and 100.0");
         }
-        this.sharpenValue = parameterValue;
+        this.sharpenValue = parameterValue;//setting the parameter value
     }
 
-    public Pixel[][] apply(Pixel[][] imageFile, String fileName, LoggingService loggingService) {
+    public Pixel[][] apply(Pixel[][] imageFile, String fileName, LoggingService loggingService) {//this function applies sharpen effect
         String optionValues = String.valueOf(sharpenValue);
-        loggingService.addLog(fileName,"Sharpen",optionValues);
-        imageFile = SharpenInterface.applySharpen(imageFile,sharpenValue);
+        loggingService.addLog(fileName,"Sharpen",optionValues);//adding to logs
+        imageFile = SharpenInterface.applySharpen(imageFile,sharpenValue);//applying the effect
         return imageFile;
     }
 }
