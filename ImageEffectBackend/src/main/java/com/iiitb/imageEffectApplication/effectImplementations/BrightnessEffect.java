07f3763cecf8334773fa3965f720e4febdbf5fc6
Pradyumna G <116896490+PradyumnaG@users.noreply.github.com>
2024-01-02 11:28:00 +0530

    Added comments

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementations/BrightnessEffect.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementations/BrightnessEffect.java
@@ -7,21 +7,21 @@ import com.iiitb.imageEffectApplication.libraryInterfaces.*;
 import com.iiitb.imageEffectApplication.libraryInterfaces.Pixel;
 import com.iiitb.imageEffectApplication.service.*;
 
-public class BrightnessEffect implements SingleValueParameterizableEffect {
+public class BrightnessEffect implements SingleValueParameterizableEffect {// implements Single Value Parameter
 
     private float brightnessValue;
 
-    public void setParameterValue(float parameterValue) throws IllegalParameterException {
-        if (parameterValue < 0.0f || parameterValue > 200.0f) {
+    public void setParameterValue(float parameterValue) throws IllegalParameterException { //this function sets the parameter value
+        if (parameterValue < 0.0f || parameterValue > 200.0f) {// throws exception if range of parameter is out of range (0,200)
             throw new IllegalParameterException("Brightness value must be between 0.0 and 200.0");
         }
-        this.brightnessValue = parameterValue;
+        this.brightnessValue = parameterValue; //setting parameter value
     }
 
-    public Pixel[][] apply(Pixel[][] imageFile, String fileName, LoggingService loggingService) {
+    public Pixel[][] apply(Pixel[][] imageFile, String fileName, LoggingService loggingService) { //function applies brightness effect
         String optionValues = String.valueOf(brightnessValue);
-        loggingService.addLog(fileName,"Brightness",optionValues);
-        imageFile = BrightnessInterface.applyBrightness(imageFile,brightnessValue/100);
+        loggingService.addLog(fileName,"Brightness",optionValues);//adding to logs
+        imageFile = BrightnessInterface.applyBrightness(imageFile,brightnessValue/100);//applying the effect
         return imageFile;
     }
 }
