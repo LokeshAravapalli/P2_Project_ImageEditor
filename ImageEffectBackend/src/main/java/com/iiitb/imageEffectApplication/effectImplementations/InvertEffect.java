7e88d728df54932d8195336943d6095ec59a755b
Pradyumna G <116896490+PradyumnaG@users.noreply.github.com>
2024-01-02 11:52:23 +0530

    Added comments

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementations/InvertEffect.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementations/InvertEffect.java
@@ -5,11 +5,11 @@ import com.iiitb.imageEffectApplication.exception.IllegalParameterException;
 import com.iiitb.imageEffectApplication.libraryInterfaces.*;
 import com.iiitb.imageEffectApplication.service.*;
 
-public class InvertEffect implements PhotoEffect{
+public class InvertEffect implements PhotoEffect{//implemets PhotoEffect interface
 
-    public Pixel[][] apply(Pixel[][] imageFile, String fileName, LoggingService loggingService) {
-        loggingService.addLog(fileName,"Invert","no value");
-        imageFile = InvertInterface.applyInvert(imageFile);
+    public Pixel[][] apply(Pixel[][] imageFile, String fileName, LoggingService loggingService) {//this effect takes no parameter value, this function applies invert effect
+        loggingService.addLog(fileName,"Invert","no value");//adding logs
+        imageFile = InvertInterface.applyInvert(imageFile);//applying the effect
         return imageFile;
     }
 }
