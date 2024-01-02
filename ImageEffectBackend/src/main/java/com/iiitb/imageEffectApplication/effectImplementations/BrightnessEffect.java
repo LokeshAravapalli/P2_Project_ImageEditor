91ac331dc60156da5b32c27036113125077ca3de
Pradyumna G <116896490+PradyumnaG@users.noreply.github.com>
2024-01-02 11:42:31 +0530

    Added comments

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementations/BrightnessEffect.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementations/BrightnessEffect.java
@@ -7,7 +7,7 @@ import com.iiitb.imageEffectApplication.libraryInterfaces.*;
 import com.iiitb.imageEffectApplication.libraryInterfaces.Pixel;
 import com.iiitb.imageEffectApplication.service.*;
 
-public class BrightnessEffect implements SingleValueParameterizableEffect {// implements Single Value Parameter
+public class BrightnessEffect implements SingleValueParameterizableEffect {// implements Single Value Parameter interface
 
     private float brightnessValue;
 
