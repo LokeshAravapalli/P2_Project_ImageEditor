fc3c36ffd057a52e1668a8806329cce7e2cbdef9
Pradyumna G <116896490+PradyumnaG@users.noreply.github.com>
2024-01-02 11:48:25 +0530

    Added comments

--- ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementations/GrayScaleEffect.java
+++ ImageEffectBackend/src/main/java/com/iiitb/imageEffectApplication/effectImplementations/GrayScaleEffect.java
@@ -5,7 +5,7 @@ import com.iiitb.imageEffectApplication.exception.IllegalParameterException;
 import com.iiitb.imageEffectApplication.libraryInterfaces.*;
 import com.iiitb.imageEffectApplication.service.*;
 
-public class GrayScaleEffect implements PhotoEffect{// implemets PhotoEffect interface
+public class GrayScaleEffect implements PhotoEffect{// implements PhotoEffect interface
 
     public Pixel[][] apply(Pixel[][] imageFile, String fileName, LoggingService loggingService) {//Grayscale takes no parameter, this function applies grayscale effect
         loggingService.addLog(fileName,"Grayscale","no value");//adding to logs
